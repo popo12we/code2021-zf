d40e9baeb3613c92bcef75df070dc39afbba7788
e <zawb-lujunhui@zhongan.io>
2021-01-14 14:38:12 +0800

    data代理

--- src/state.js
+++ src/state.js
@@ -1,11 +1,26 @@
 import { isFunction } from './utils'
 import { observe } from './observer/index'
+function proxy (vm, source, key) {
+    Object.defineProperty(vm, key, {
+        get () {
+            return vm[source][key]
+        },
+        set (newvalue) {
+            newvalue = vm[source][key]
+        }
+    })
+}
 function initState (vm) {
 
     // 对data进行函数对象化操作
     let data = vm.$options.data
     data = vm._data = isFunction(data) ? data.call(vm) : data
-    //数据劫持
+    //代理对象  
+    //数据劫持 可以做到访问vm._data.age相当于vm.age
+
+    for (let key in data) {
+        proxy(vm, '_data', key)
+    }
     observe(data)
 }
 export default initState
